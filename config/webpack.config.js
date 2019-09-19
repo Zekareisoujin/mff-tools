a8fc171e4b8a43d4c537812e4ddf41f224c99761
Zekareisoujin <Zeka304@gmail.com>
2019-09-20 02:49:22 +0800

    chore: remove handholding scripts, added webpack & babel configs

---
+++ config/webpack.config.js
@@ -0,0 +1,43 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_module/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.html$/,
+        use: 'html-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader'
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: {
+          loader: 'url-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ],
+  devServer: {
+    contentBase: './public'
+  }
+};
